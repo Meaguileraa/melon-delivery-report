735031a1791822734af08aee0f896117534175cb
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-16 19:29:36 -0700

    Less repetitive/ FInal Commit

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,19 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def melon_delivery(day, file):
+    """Given day number & path to the deliveries, produce report."""
+    print(f'Day {day}')
+    the_file = open(file)
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    for line in the_file: 
+        line = line.rstrip()
+        words = line.split('|')
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        print(f'Delivered {count} {melon}s for a total of ${amount}')
+        
+    the_file.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+melon_delivery(1, "um-deliveries-20140519.txt")
+melon_delivery(2, "um-deliveries-20140520.txt")
+melon_delivery(3, "um-deliveries-20140521.txt")
